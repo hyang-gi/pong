0228e27a336579edda0761e2b6e68e62c513c644
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2022-12-08 10:36:24 +0000

    centered the ball to middle of canvas

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
   console.log("It works!");
+
+  let ballX = 300;
+  let ballY = 200;
+
+  $("#ball").css("left", `${ballX}px`);
+  $("#ball").css("top", `${ballY}px`);
+  
 });
