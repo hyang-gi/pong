f0792d58fb33c1814db6b6ee82d992e7d843d5f9
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2022-12-09 23:40:55 +0000

    added mover function to ball

--- main.js
+++ main.js
@@ -1,13 +1,64 @@
+const b = {
+		x:400,
+		y:200,
+		w:40,
+		h:40,
+		dx:1,
+		dy:1,
+		speed:5,
+		ani:{}
+	}
+
+  function mover(){
+    b.x += b.dx * b.speed;
+    b.y += b.dy * b.speed;
+  
+    // console.log("moving");
+    
+    $("#ball").css({left: `${b.x}px`});
+    $("#ball").css({top: `${b.y}px`});
+  
+    if(b.x > 840-b.w || b.x < 0){
+      b.dx *= -1;
+    }
+  
+    if(b.y > 480 - b.h || b.y < 0){
+      b.dy *= -1;
+    }
+  
+    b.ani = window.requestAnimationFrame(mover);
+    
+  }
+
+// canvas and ball is fixed entity of the game, hence no class is used for the same
+// class to create paddles
+
+class Paddle {
+
+  constructor(x, y, size, speed, colour, index) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.speed = speed;
+    this.dx = 1;
+    this.dy = 1;
+    this.colour = colour;
+    this.index = index;
+  }
+}
+
 $(document).ready(function () {
   console.log("It works!");
+  $("#ball").css("left", `${b.x}px`);
+  $("#ball").css("top", `${b.y}px`);
 
-  let ballX = 400;
-  let ballY = 200;
-
-  $("#ball").css("left", `${ballX}px`);
-  $("#ball").css("top", `${ballY}px`);
+  $(document).on('click', 'button', function(event){
+		event.preventDefault();
+		b.ani = window.requestAnimationFrame(mover);	
+	});
 
 });
+
 
 /*TODO 
 
