74f5af30b8daf62461564b41696f47743f919efe
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2022-12-13 18:58:34 +0000

    replaced variable name to understandable value

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const b = {
+const ball = {
   x: 400,
   y: 200,
   w: 40,
@@ -11,22 +11,22 @@ const b = {
 
 function mover() {
   //console.log("mover function");
-  b.x += b.dx * b.speed;
-  b.y += b.dy * b.speed;
+  ball.x += ball.dx * ball.speed;
+  ball.y += ball.dy * ball.speed;
 
   //to move the ball with new coordinates
-  $("#ball").css({ left: `${b.x}px` });
-  $("#ball").css({ top: `${b.y}px` });
+  $("#ball").css({ left: `${ball.x}px` });
+  $("#ball").css({ top: `${ball.y}px` });
 
-  if (b.x > 840 - b.w || b.x < 0) {
-    b.dx *= -1;
+  if (ball.x > 840 - ball.w || ball.x < 0) {
+    ball.dx *= -1;
   }
 
-  if (b.y > 480 - b.h || b.y < 0) {
-    b.dy *= -1;
+  if (ball.y > 480 - ball.h || ball.y < 0) {
+    ball.dy *= -1;
   }
 
-  b.ani = window.requestAnimationFrame(mover);
+  ball.ani = window.requestAnimationFrame(mover);
 
 }
 
@@ -99,8 +99,8 @@ function getColour() {
 
 $(document).ready(function () {
   console.log("It works!");
-  $("#ball").css("left", `${b.x}px`);
-  $("#ball").css("top", `${b.y}px`);
+  $("#ball").css("left", `${ball.x}px`);
+  $("#ball").css("top", `${ball.y}px`);
 
   const paddle1 = new Paddle(10, 160, 25, 100, getColour(), 'paddle1');
   paddle1.show();
@@ -110,7 +110,7 @@ $(document).ready(function () {
 
   $(document).on('click', 'button', function (event) {
     event.preventDefault();
-    b.ani = window.requestAnimationFrame(mover);
+    ball.ani = window.requestAnimationFrame(mover);
     paddle1.move();
   });
 
