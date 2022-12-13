3b5dcaef4c5cad37ca65a09a97dfbfb464925a7b
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2022-12-13 14:06:57 +0000

    removed comments

--- main.js
+++ main.js
@@ -95,21 +95,3 @@ $(document).ready(function () {
   });
 
 });
-
-
-/*TODO 
-
-Create paddles
-Move the ball -- done 
-Collision should bounce the ball back
-Set up a counter
-if ball goes behind paddles, decrease counter
-add event listener to paddles
-movement of one of the paddles is computer generated
-event listener for start game, reset game
-
-#issues
-
-paddle position is not being set
-
-*/
